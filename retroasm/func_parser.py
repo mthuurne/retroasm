94e10506c30cc6cf98269b303aaec93cc7870506
Maarten ter Huurne <maarten@treewalker.org>
2014-12-21 04:17:32 +0100

    instr: Do not warn about unused arguments if a function contains errors
    This is to avoid false positives: errors in a function body means not
    all code was emitted, which may very well be the reason why arguments
    are not used. In any case, the user has something more important to
    focus on.
    
    As part of this change, Function objects will now contain None for
    their 'code' attribute if there were errors in the body. I think that
    will produce the best results when we deal with function calls later:
    since there is a Function object, we can verify the arguments passed
    to it. But a partial body might lead to false positives when trying to
    merge it into the calling code.

--- retroasm/func_parser.py
+++ retroasm/func_parser.py
@@ -2,6 +2,7 @@ from .codeblock import (
     ArgumentConstant, CodeBlockBuilder, emitCodeFromAssignments
     )
 from .expression import LocalReference, LocalValue
+from .linereader import DelayedError
 
 class Function:
 
@@ -23,7 +24,10 @@ class Function:
 
     def dump(self):
         print(str(self))
-        self.code.dump()
+        if self.code is None:
+            print('    no code')
+        else:
+            self.code.dump()
 
     def findArg(self, argName):
         '''Searches the representation of the argument with the given name in
@@ -32,8 +36,11 @@ class Function:
         either a LocalValue or an ArgumentConstant is returned. If the argument
         does not occur in the code block, None is returned. If no argument with
         the given name existed when the function was created, KeyError is
+        raised. If this Function doesn't have a code block, ValueError is
         raised.
         '''
+        if self.code is None:
+            raise ValueError('Function does not have a code block')
         arg = self.args[argName]
         if isinstance(arg, LocalValue):
             # If the code block was simplified, the LocalValue will have been
@@ -49,22 +56,28 @@ class Function:
                     return ref
         return None
 
-def createFunc(log, name, args, assignments):
-    headerLocation = log.getLocation()
+def createFunc(reader, name, args, assignments):
+    headerLocation = reader.getLocation()
     builder = CodeBlockBuilder()
-    emitCodeFromAssignments(log, builder, assignments)
-    code = builder.createCodeBlock()
-    code.simplify()
+    try:
+        with reader.checkErrors():
+            emitCodeFromAssignments(reader, builder, assignments)
+    except DelayedError:
+        code = None
+    else:
+        code = builder.createCodeBlock()
+        code.simplify()
     func = Function(name, args, code)
 
-    # Warn about unused arguments.
-    # Note that simplification can remove usage that has no effect on execution,
-    # but it is probably a good idea to warn about that too.
-    for argName in args.keys():
-        if func.findArg(argName) is None:
-            log.warning(
-                'unused argument "%s" in function "%s"',
-                argName, name, location=headerLocation
-                )
+    if code is not None:
+        # Warn about unused arguments.
+        # Note that simplification can remove usage that has no effect on
+        # execution, but it is probably a good idea to warn about that too.
+        for argName in args.keys():
+            if func.findArg(argName) is None:
+                reader.warning(
+                    'unused argument "%s" in function "%s"',
+                    argName, name, location=headerLocation
+                    )
 
     return func
