585167f443f08ae9a836f74cf262bab4f5a3aed2
Maarten ter Huurne <maarten@treewalker.org>
2015-01-10 20:00:30 +0100

    instr: Use enum to identify the kind of token
    Enum is part of Python 3.4 and can be installed as the "enum34" package
    in older Python versions.

--- retroasm/expression_parser.py
+++ retroasm/expression_parser.py
@@ -7,6 +7,7 @@ from .function import Function, FunctionCall
 from .storage import IOChannel, IOReference
 from .types import IntType, Reference, unlimited
 
+from enum import Enum
 import re
 
 def parseType(typeName):
@@ -25,19 +26,26 @@ def parseTypeDecl(typeDecl):
     else:
         return parseType(typeDecl)
 
+Token = Enum('Token', ( # pylint: disable=invalid-name
+    'identifier', 'number', 'operator', 'bracket', 'assignment', 'separator',
+    'whitespace', 'other', 'end'
+    ))
+
 class ExpressionTokenizer:
 
-    _pattern = re.compile('|'.join('(?P<%s>%s)' % pair for pair in (
-        # pylint: disable=bad-whitespace
-        ('identifier',  r"[A-Za-z_][A-Za-z0-9_]*'?"),
-        ('number',      r'[%$0-9]\w*'),
-        ('operator',    r'[&|\^+\-~!;]|==|!='),
-        ('bracket',     r'[\[\]()]'),
-        ('assignment',  r':='),
-        ('separator',   r'[:,]'),
-        ('whitespace',  r'\s+'),
-        ('other',       r'.'),
-        )))
+    _pattern = re.compile('|'.join(
+        '(?P<%s>%s)' % (token.name, regex) for token, regex in (
+            # pylint: disable=bad-whitespace
+            (Token.identifier,  r"[A-Za-z_][A-Za-z0-9_]*'?"),
+            (Token.number,      r'[%$0-9]\w*'),
+            (Token.operator,    r'[&|\^+\-~!;]|==|!='),
+            (Token.bracket,     r'[\[\]()]'),
+            (Token.assignment,  r':='),
+            (Token.separator,   r'[:,]'),
+            (Token.whitespace,  r'\s+'),
+            (Token.other,       r'.'),
+            )
+        ))
 
     def __init__(self, exprStr):
         self._tokens = self._pattern.finditer(exprStr)
@@ -48,20 +56,20 @@ class ExpressionTokenizer:
             try:
                 match = next(self._tokens)
             except StopIteration:
-                self.kind = 'end'
+                self.kind = Token.end
                 self.value = None
                 break
-            kind = match.lastgroup
-            if kind != 'whitespace':
+            kind = getattr(Token, match.lastgroup)
+            if kind is not Token.whitespace:
                 self.kind = kind
-                self.value = match.group(kind)
+                self.value = match.group(kind.name)
                 break
 
     def peek(self, kind, value=None):
         '''Returns True if the current token matches the given kind and,
         if specified, also the given value, False otherwise.
         '''
-        return self.kind == kind and (value is None or self.value == value)
+        return self.kind is kind and (value is None or self.value == value)
 
     def eat(self, kind, value=None):
         '''Consumes the current token if it matches the given kind and,
@@ -79,34 +87,34 @@ def _parse(exprStr, context, statement):
     class BadToken(ValueError):
         def __init__(self, where, expected):
             msg = 'bad %s expression: expected %s, got %s "%s"' % (
-                where, expected, token.kind, token.value
+                where, expected, token.kind.name, token.value
                 )
             ValueError.__init__(self, msg)
 
     def parseAssign():
         expr = parseTop()
-        if token.eat('assignment', ':='):
+        if token.eat(Token.assignment, ':='):
             return Assignment(expr, parseTop())
         else:
             return expr
 
     def parseOr():
         expr = parseXor()
-        if token.eat('operator', '|'):
+        if token.eat(Token.operator, '|'):
             return OrOperator(expr, parseOr())
         else:
             return expr
 
     def parseXor():
         expr = parseAnd()
-        if token.eat('operator', '^'):
+        if token.eat(Token.operator, '^'):
             return XorOperator(expr, parseXor())
         else:
             return expr
 
     def parseAnd():
         expr = parseAddSub()
-        if token.eat('operator', '&'):
+        if token.eat(Token.operator, '&'):
             return AndOperator(expr, parseAnd())
         else:
             return expr
@@ -114,9 +122,9 @@ def _parse(exprStr, context, statement):
     def parseAddSub():
         exprs = [parseConcat()]
         while True:
-            if token.eat('operator', '+'):
+            if token.eat(Token.operator, '+'):
                 exprs.append(parseConcat())
-            elif token.eat('operator', '-'):
+            elif token.eat(Token.operator, '-'):
                 exprs.append(Complement(parseConcat()))
             else:
                 break
@@ -124,12 +132,12 @@ def _parse(exprStr, context, statement):
 
     def parseConcat():
         exprs = [parseUnary()]
-        while token.eat('operator', ';'):
+        while token.eat(Token.operator, ';'):
             exprs.append(parseUnary())
         return exprs[0] if len(exprs) == 1 else Concatenation(*exprs)
 
     def parseUnary():
-        if token.eat('operator', '-'):
+        if token.eat(Token.operator, '-'):
             return Complement(parseUnary())
         else:
             return parseSlice()
@@ -138,16 +146,16 @@ def _parse(exprStr, context, statement):
         expr = parseGroup()
         if isinstance(expr, IOChannel):
             return parseIOReference(expr)
-        if not token.eat('bracket', '['):
+        if not token.eat(Token.bracket, '['):
             return expr
 
         # Bitwise lookup or bit string slicing.
-        if token.peek('separator', ':'):
+        if token.peek(Token.separator, ':'):
             start = IntLiteral.create(0)
         else:
             start = parseTop()
-        if token.eat('separator', ':'):
-            if token.peek('bracket', ']'):
+        if token.eat(Token.separator, ':'):
+            if token.peek(Token.bracket, ']'):
                 if expr.width is unlimited:
                     raise ValueError(
                         'omitting the end index not allowed when slicing '
@@ -156,9 +164,9 @@ def _parse(exprStr, context, statement):
                 end = IntLiteral.create(expr.width)
             else:
                 end = parseTop()
-            if not token.eat('bracket', ']'):
+            if not token.eat(Token.bracket, ']'):
                 raise BadToken('slice', '"]"')
-        elif token.eat('bracket', ']'):
+        elif token.eat(Token.bracket, ']'):
             end = None
         else:
             raise BadToken('slice/lookup', '":" or "]"')
@@ -183,31 +191,31 @@ def _parse(exprStr, context, statement):
 
     def parseIOReference(channel):
         # I/O lookup.
-        if not token.eat('bracket', '['):
+        if not token.eat(Token.bracket, '['):
             raise BadToken('I/O reference', '"["')
         index = parseTop()
-        if not token.eat('bracket', ']'):
+        if not token.eat(Token.bracket, ']'):
             raise BadToken('I/O index', '"]"')
         return IOReference(channel, index)
 
     def parseGroup():
-        if token.eat('bracket', '('):
+        if token.eat(Token.bracket, '('):
             expr = parseTop()
-            if not token.eat('bracket', ')'):
+            if not token.eat(Token.bracket, ')'):
                 raise BadToken('parenthesized', ')')
             return expr
         else:
             return parseIdent()
 
     def parseIdent():
-        if token.kind == 'number':
+        if token.kind is Token.number:
             return parseNumber()
-        if token.kind != 'identifier':
+        if token.kind is not Token.identifier:
             raise BadToken('innermost', 'identifier')
 
         name = token.value
         next(token)
-        if token.eat('bracket', '('):
+        if token.eat(Token.bracket, '('):
             # Function call.
             try:
                 func = context[name]
@@ -217,7 +225,7 @@ def _parse(exprStr, context, statement):
                 raise ValueError('"%s" is not a function' % name)
             args = parseFuncArgs()
             return FunctionCall(func, args)
-        elif token.kind == 'identifier':
+        elif token.kind is Token.identifier:
             # Two identifiers in a row means the first is a type declaration.
             typ = parseType(name)
             name = token.value
@@ -240,12 +248,12 @@ def _parse(exprStr, context, statement):
 
     def parseFuncArgs():
         args = []
-        if not token.eat('bracket', ')'):
+        if not token.eat(Token.bracket, ')'):
             while True:
                 args.append(parseTop())
-                if token.eat('bracket', ')'):
+                if token.eat(Token.bracket, ')'):
                     break
-                if not token.eat('separator', ','):
+                if not token.eat(Token.separator, ','):
                     raise BadToken('function call arguments', '"," or ")"')
         return args
 
@@ -271,13 +279,14 @@ def _parse(exprStr, context, statement):
     parseTop = parseOr
 
     expr = parseAssign() if statement else parseTop()
-    if token.kind == 'other':
+    if token.kind is Token.other:
         raise ValueError(
             'unexpected character "%s" in expression' % token.value
             )
-    elif token.kind != 'end':
+    elif token.kind is not Token.end:
         raise ValueError(
-            'found %s "%s" in an unexpected place' % (token.kind, token.value)
+            'found %s "%s" in an unexpected place'
+            % (token.kind.name, token.value)
             )
     else:
         return expr
