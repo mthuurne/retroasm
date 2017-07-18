mthuurne/retroasm

commit dd9e7aeb3c1eed62cab36827adb6ab2c254a2022

instr: Treat placeholder value more like a function argument
Functions have a local variable for each value argument, which is
initialized to the passed value, but can be changed by the function
body.

Now the semantics field works in a similar way. Even though there
can be only one statement in the semantics field, there is an
observable difference when a reference to a placeholder is passed
to a function: previously anything written to that reference would
be discarded, while now it persists (with a life time limited to
a single semantics evaluation).

Since the encoding builder is not allowed to emit any loads, that
will still emit a FixedValue instead of a local variable.

retroasm/instrset_parser.py
@@ -294,9 +294,7 @@ def _buildPlaceholder(placeholder, typ, builder):
     elif isinstance(typ, ReferenceType):
         builder.emitReferenceArgument(name, typ.type, decl.name.location)
     else:
-        immediate = ArgumentValue(name, typ.mask)
-        ref = FixedValue(immediate, typ)
-        builder.defineReference(name, ref, decl.name.location)
+        builder.emitValueArgument(name, typ, decl.name.location)
 
 def _parseModeEncoding(encNodes, encBuilder, placeholderSpecs, reader):
     # Define placeholders in encoding builder.

retroasm/codeblock_builder.py
@@ -109,10 +109,22 @@ class LocalCodeBlockBuilder(CodeBlockBuilder):
         namespace = LocalNamespace(self, parentBuilder)
         CodeBlockBuilder.__init__(self, namespace)
 
+    def emitValueArgument(self, name, typ, location):
+        '''Adds a passed-by-value argument to this code block.
+        The initial value is represented by an ArgumentValue and is stored
+        into a corresponding local Variable that is created by this method.
+        Returns the Reference to the corresponding Variable.
+        '''
+        raise NotImplementedError
+
 class EncodingCodeBlockBuilder(
         StatelessCodeBlockBuilderMixin, LocalCodeBlockBuilder
         ):
-    pass
+
+    def emitValueArgument(self, name, typ, location):
+        checkType(typ, IntType, 'value argument')
+        value = ArgumentValue(name, maskForWidth(typ.width))
+        return self.defineReference(name, FixedValue(value, typ), location)
 
 class SemanticsCodeBlockBuilder(LocalCodeBlockBuilder):
 
@@ -165,17 +177,17 @@ class SemanticsCodeBlockBuilder(LocalCodeBlockBuilder):
         code.freeze()
         return code
 
-    def emitValueArgument(self, name, decl, location):
+    def emitValueArgument(self, name, typ, location):
         '''Adds a passed-by-value argument to this code block.
         The initial value is represented by an ArgumentValue and is stored
         into the corresponding Variable.
         Returns the Reference to the corresponding Variable.
         '''
-        assert isinstance(decl, IntType), decl
-        value = ArgumentValue(name, maskForWidth(decl.width))
+        checkType(typ, IntType, 'value argument')
+        value = ArgumentValue(name, maskForWidth(typ.width))
 
         # Add Variable.
-        ref = self.emitVariable(name, decl, location)
+        ref = self.emitVariable(name, typ, location)
 
         # Store initial value.
         ref.emitStore(value, location)

commit 25d37cddb5dbc09f7932ebaefcacfe8fe8c66793

instr: Fix computation of context-defined placeholder value
This was broken since dd9e7aeb (Treat placeholder value more like
a function argument). That commit puts a local variable into the
namespace rather than the argument itself, which resulted in
EncodeMatch not being able to simplify the placeholder value into
an IntLiteral.

Most of the diff is in moving the placeholder construction code
to combine it with the emitting. The actual change is the use of
createCodeBlock() with an overridden return reference name.

retroasm/instrset_parser.py
@@ -755,12 +755,30 @@ def _parseModeEntries(
                 else:
                     placeholderSpecs, flagsRequired = {}, set()
 
-                # Define placeholders in context builder.
+                # Compute semantics for placeholders.
                 ctxBuilder = SemanticsCodeBlockBuilder(namespace)
+                placeholders = {}
                 try:
-                    for spec in placeholderSpecs.values():
+                    for name, spec in placeholderSpecs.items():
                         semType = spec.semanticsType
                         _buildPlaceholder(spec, semType, ctxBuilder)
+                        if isinstance(spec, ValuePlaceholderSpec):
+                            if spec.value is None:
+                                value = None
+                            else:
+                                code = ctxBuilder.createCodeBlock(name)
+                                ref = code.retRef
+                                # TODO: While the documentation says we do support
+                                #       defining references in the context, the
+                                #       parse code rejects "<type>&".
+                                assert isinstance(ref, FixedValue), ref
+                                value = ref.expr
+                            placeholder = ValuePlaceholder(name, semType, value)
+                        elif isinstance(spec, MatchPlaceholderSpec):
+                            placeholder = MatchPlaceholder(name, spec.mode)
+                        else:
+                            assert False, spec
+                        placeholders[name] = placeholder
                 except BadInput as ex:
                     reader.error(
                         'error in context: %s', ex, location=ex.location
@@ -798,28 +816,6 @@ def _parseModeEntries(
                     decoding = _parseModeDecoding(
                         encoding, encBuilder, placeholderSpecs, reader
                         )
-
-                # Convert placeholders.
-                placeholders = {}
-                for name, spec in placeholderSpecs.items():
-                    if isinstance(spec, ValuePlaceholderSpec):
-                        if spec.value is None:
-                            value = None
-                        else:
-                            ref = ctxBuilder.namespace[name]
-                            # TODO: While the documentation says we do support
-                            #       defining references in the context, the
-                            #       parse code rejects "<type>&".
-                            assert isinstance(ref, FixedValue), ref
-                            value = ref.expr
-                        placeholder = ValuePlaceholder(
-                            name, spec.semanticsType, value
-                            )
-                    elif isinstance(spec, MatchPlaceholderSpec):
-                        placeholder = MatchPlaceholder(name, spec.mode)
-                    else:
-                        assert False, spec
-                    placeholders[name] = placeholder
 
                 # Parse mnemonic.
                 mnemonic = mnemBase + tuple(_parseMnemonic(