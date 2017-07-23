88e00f0645e4eb91a8c7246ab4cf128f5e4ea79a
Maarten ter Huurne <maarten@treewalker.org>
2017-07-23 12:38:04 +0200

    instr: Add consistency check for function arguments
    When the instruction set definition parser is used, it should be
    impossible to create these inconsistencies, but since bugs might
    exist it is useful to check anyway. Also Functions can be created
    without using the parser.

--- retroasm/function.py
+++ retroasm/function.py
@@ -1,9 +1,12 @@
+from .codeblock import ArgumentValue
+from .storage import RefArgStorage
 from .types import ReferenceType
 
 class Function:
 
     def __init__(self, name, retType, args, code):
         if code is not None:
+            _checkArgs(args, code.arguments)
             _checkReturn(retType, code.retRef)
 
         self.name = name
@@ -32,6 +35,34 @@ class Function:
             print('    no code')
         else:
             self.code.dump()
+
+def _checkArgs(declArgs, codeArgs):
+    '''Check consistency between declared argument types and code block.
+    Raises ValueError if an inconsistency is found.
+    '''
+    for name, arg in codeArgs.items():
+        try:
+            typ = declArgs[name]
+        except KeyError:
+            raise ValueError('code block uses undeclared argument "%s"' % name)
+
+        if isinstance(typ, ReferenceType):
+            if not isinstance(arg, RefArgStorage):
+                raise ValueError(
+                    'reference argument "%s" is not a reference in code block'
+                    % name
+                    )
+            if typ.type != arg.type:
+                raise ValueError(
+                    'reference argument "%s" is declared with type %s but '
+                    'has type %s in code block' % (name, typ.type, arg.type)
+                    )
+        else:
+            if not isinstance(arg, ArgumentValue):
+                raise ValueError(
+                    'value argument "%s" is not a value in code block'
+                    % name
+                    )
 
 def _checkReturn(retType, retRef):
     '''Check consistency between declared return type and code block.
