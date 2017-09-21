mthuurne/retroasm

commit 43b9cb9af7997a614369597c0d874a1e5e3c797d

instr: Add fast path in inlineBlock() where no substitution takes place

retroasm/codeblock_builder.py
@@ -143,7 +143,7 @@ class SemanticsCodeBlockBuilder(CodeBlockBuilder):
             assert len(returned) == 0, returned
             return None
 
-    def inlineBlock(self, code, argFetcher):
+    def inlineBlock(self, code, argFetcher=None):
         '''Inlines another code block into this one.
         The given argument fetcher function, when called with an argument name,
         should return the bit string passed for that argument, or None if the
@@ -151,6 +151,11 @@ class SemanticsCodeBlockBuilder(CodeBlockBuilder):
         Returns a list of BitStrings containing the values returned by the
         inlined block.
         '''
+        if argFetcher is None:
+            # No substitution takes place, so we can copy nodes and returned
+            # bits as-is.
+            self.nodes += code.nodes
+            return code.returned
 
         loadResults = {}
         def substExpr(expr):

commit 0426b7c2f8638cecdfd09934695b829b22d04a32

instr: Fix subtle bug in optimization attempt in inlineBlock()
This bug was introduced in 43b9cb9a (instr: Add fast path in
inlineBlock() where no substitution takes place).

A test case was added to demonstrate the bug.

Loads are identified by the Load node object itself. So if a Load
object is added to more than one node list, it is seen as the exact
same load occurring at multiple moments in time, which is incorrect.

For the moment I removed the fast path. Maybe I'll want to add
a correct fast path at some point in the future, but right now
I'm not focusing on this part of the code.

retroasm/codeblock_builder.py
@@ -162,10 +162,7 @@ class SemanticsCodeBlockBuilder(CodeBlockBuilder):
         inlined block.
         '''
         if argFetcher is None:
-            # No substitution takes place, so we can copy nodes and returned
-            # bits as-is.
-            self.nodes += code.nodes
-            return code.returned
+            argFetcher = lambda name: None
 
         loadResults = {}
         def importExpr(expr):

test/unittest/test_codeblock_inline.py
@@ -1,7 +1,7 @@
 from utils_codeblock import NodeChecker, TestNamespace
 
 from retroasm.codeblock import Load, Store
-from retroasm.expression import AddOperator, IntLiteral
+from retroasm.expression import AddOperator, IntLiteral, XorOperator
 from retroasm.function import Function
 from retroasm.reference import (
     ConcatenatedBits, FixedValue, Reference, SlicedBits
@@ -418,6 +418,43 @@ class CodeBlockInlineTests(NodeChecker, unittest.TestCase):
         retVal, retWidth = self.getRetVal(code)
         self.assertEqual(retWidth, 8)
         self.assertEqual(retVal, code.nodes[0].expr)
+
+    def test_unique_loads(self):
+        '''Test whether multiple instances of the same load are kept separate.
+        If the possibility of side effects is ignored, only one load will
+        remain.
+        If the load results are not kept separate, the load result will be
+        XOR-ed with itself, resulting in a 0 result.
+        '''
+        addr = IntLiteral(0xFFFF)
+
+        inner = TestNamespace()
+        memByte = inner.addIOStorage('mem', addr)
+        loadR = inner.emitLoad(memByte)
+        innerRet = inner.addVariable('ret')
+        inner.emitStore(innerRet, loadR)
+        innerCode = inner.createCodeBlock()
+        self.assertEqual(len(innerCode.returned), 1)
+
+        outer = TestNamespace()
+        val1Bits, = outer.inlineBlock(innerCode, None)
+        self.assertIsInstance(val1Bits, FixedValue)
+        val1 = val1Bits.expr
+        val2Bits, = outer.inlineBlock(innerCode, None)
+        self.assertIsInstance(val2Bits, FixedValue)
+        val2 = val2Bits.expr
+        outerRet = outer.addVariable('ret')
+        outer.emitStore(outerRet, XorOperator(val1, val2))
+
+        code = createSimplifiedCode(outer)
+        correct = (
+            Load(memByte.bits.storage),
+            Load(memByte.bits.storage),
+            )
+        self.assertNodes(code.nodes, correct)
+        retVal, retWidth = self.getRetVal(code)
+        self.assertEqual(retWidth, 8)
+        self.assertIsInstance(retVal, XorOperator)
 
 if __name__ == '__main__':
     verbose = True