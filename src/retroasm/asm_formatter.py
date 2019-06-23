1197a30d3210ea6d4e748e1fe036667f086942dc
Maarten ter Huurne <maarten@treewalker.org>
2019-06-23 20:42:59 +0200

    Complete type annotations of 'asm_formatter' module

--- src/retroasm/asm_formatter.py
+++ src/retroasm/asm_formatter.py
@@ -1,5 +1,9 @@
+from typing import AbstractSet, Iterable, Mapping, Union
+
+from .expression import IntLiteral
 from .mode import PlaceholderRole
-from .reference import Reference
+from .reference import FixedValue, Reference
+from .types import IntType, Width, unlimited
 
 
 class Formatter:
@@ -10,14 +14,20 @@ class Formatter:
     def __init__(self) -> None:
         self._lineFormat = '{:%d}{:%d}{}' % (self.margin, self.operationWidth)
 
-    def formatInt(self, value, typ):
-        return (
-            '{:d}'
-            if value < 16 or typ.signed else
-            '${:0%dx}' % ((typ.width + 3) // 4)
-            ).format(value)
+    def formatInt(self, value: int, typ: IntType) -> str:
+        if value < 16 or typ.signed:
+            return str(value)
+        else:
+            width = typ.width
+            if width is unlimited:
+                # The type doesn't tell us how wide the presentation
+                # should be, so look at the value instead.
+                width = value.bit_length()
+            else:
+                assert isinstance(width, int)
+            return ('${:0%dx}' % ((width + 3) // 4)).format(value)
 
-    def _formatOperands(self, operands):
+    def _formatOperands(self, operands: Iterable[str]) -> str:
         prevWord = False
         parts = []
         for operand in operands:
@@ -28,18 +38,29 @@ class Formatter:
             prevWord = isWord
         return ''.join(parts)
 
-    def formatLabel(self, label):
+    def formatLabel(self, label: str) -> str:
         return label + ':'
 
-    def formatMnemonic(self, mnemonic, labels):
+    def formatMnemonic(self,
+                       # TODO: Use the Mnemonic class instead?
+                       mnemonic: Iterable[Union[str, Reference]],
+                       labels: Mapping[int, str]
+                       ) -> str:
         parts = []
         for mnemElem in mnemonic:
             if isinstance(mnemElem, str):
                 parts.append(mnemElem)
             elif isinstance(mnemElem, Reference):
-                value = mnemElem.bits.expr.value
+                # TODO: Are these asserts always true? If so, try to change
+                #       the argument types to refrect that. If not, make
+                #       this method more versatile.
+                bits = mnemElem.bits
+                assert isinstance(bits, FixedValue)
+                expr = bits.expr
+                assert isinstance(expr, IntLiteral)
+                value = expr.value
                 # TODO: Role detection needs to be re-implemented.
-                roles = frozenset()
+                roles: AbstractSet[PlaceholderRole] = frozenset()
                 label = (
                     labels.get(value)
                     if PlaceholderRole.code_addr in roles or
@@ -58,6 +79,10 @@ class Formatter:
             localLabel, parts[0], self._formatOperands(parts[1:])
             ).rstrip()
 
-    def formatData(self, ref):
-        directive = {8: 'db', 16: 'dw', 32: 'dd', 64: 'dq'}[ref.width]
+    dataDirectives: Mapping[Width, str] = {
+        8: 'db', 16: 'dw', 32: 'dd', 64: 'dq'
+        }
+
+    def formatData(self, ref: Reference) -> str:
+        directive = self.dataDirectives[ref.width]
         return self.formatMnemonic((directive, ref), {})
