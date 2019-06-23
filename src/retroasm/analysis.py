52e96929268bff776f70d61648bcbedb1d3c4cb0
Maarten ter Huurne <maarten@treewalker.org>
2019-06-23 18:07:35 +0200

    Complete type annotations of 'analysis' module

--- src/retroasm/analysis.py
+++ src/retroasm/analysis.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 from collections import OrderedDict
+from typing import TYPE_CHECKING, Mapping, Optional
 
 from .codeblock import CodeBlock
 from .codeblock_builder import SemanticsCodeBlockBuilder
@@ -6,20 +9,27 @@ from .reference import BitString, SingleStorage
 from .storage import ArgStorage
 from .utils import checkType
 
+if TYPE_CHECKING:
+    from .mode import Placeholder, ModeEntry
+
 
 class CodeTemplate:
     '''A container for a code block which contains placeholders that will be
     filled in later.
     '''
 
-    def __init__(self, code, placeholders, pcBits=None):
+    def __init__(self,
+                 code: Optional[CodeBlock],
+                 placeholders: OrderedDict[str, Placeholder],
+                 pcBits: Optional[BitString] = None
+                 ):
         self.code = checkType(code, (CodeBlock, type(None)), 'code block')
-        self.placeholders = checkType(placeholders, OrderedDict, 'placeholders')
+        self.placeholders = placeholders
         self.pcBits = checkType(
             pcBits, (BitString, type(None)), 'program counter'
             )
 
-    def fillPlaceholder(self, name, entry):
+    def fillPlaceholder(self, name: str, entry: ModeEntry) -> CodeTemplate:
         '''Returns a new CodeTemplate, which is a copy of this one but with
         the match placeholder of the given name replaced by the semantics
         of the given mode entry.
@@ -35,16 +45,15 @@ class CodeTemplate:
             # TODO: Support fillCode semantics with side effects.
             assert len(fillCode.nodes) == 0, entry
 
-            def argFetcher(argName):
-                if argName == name:
-                    return fillCode.returned[0]
+            def argFetcher(argName: str) -> Optional[BitString]:
+                return fillCode.returned[0] if argName == name else None
             builder = SemanticsCodeBlockBuilder()
             returned = builder.inlineBlock(code, argFetcher)
             newCode = builder.createCodeBlock(returned)
 
         return CodeTemplate(newCode, placeholders, self.pcBits)
 
-    def rename(self, nameMap):
+    def rename(self, nameMap: Mapping[str, str]) -> CodeTemplate:
         '''Returns a new CodeTemplate, in which all placeholder names are
         substituted by their value in the given mapping.
         '''
