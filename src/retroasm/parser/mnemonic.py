0613ce966bec3ba15b16a114fdac945816967647
Maarten ter Huurne <maarten@treewalker.org>
2025-05-26 07:35:18 +0200

    Tokenize mnemonic definitions using `Tokenizer` class

--- src/retroasm/parser/mnemonic.py
+++ src/retroasm/parser/mnemonic.py
@@ -4,7 +4,6 @@ Parser for mnemonic column in instruction set definition.
 
 from __future__ import annotations
 
-import re
 from collections.abc import Iterable, Iterator
 
 from ..input import (
@@ -18,9 +17,22 @@ from ..mode import (
 )
 from ..reference import int_reference
 from ..types import IntType
+from ..utils import bad_type
 from .expression_nodes import parse_int
+from .tokens import TokenEnum, Tokenizer
 
-_re_mnemonic = re.compile(r"\w+'?|[$%]\w+|[^\w\s]")
+
+class MnemonicToken(TokenEnum):
+    identifier = r"[A-Za-z_][A-Za-z0-9_]*'?"
+    number = r"[%$0-9]\w*"
+    operator = r"[+\-*/&|^=<>@]+"
+    bracket = r"[\[\](){}]"
+    separator = r","
+    other = r".+?"
+
+
+class MnemonicTokenizer(Tokenizer[MnemonicToken]):
+    pass
 
 
 def parse_mnemonic(
@@ -30,28 +42,40 @@ def parse_mnemonic(
 ) -> Iterator[MnemItem]:
     placeholder_map = {p.name: p for p in placeholders}
     seen_placeholders: dict[str, InputLocation] = {}
-    for mnem_elem in mnem_loc.find_locations(_re_mnemonic):
-        text = mnem_elem.text
-        placeholder = placeholder_map.get(text)
-        if placeholder is None:
-            if "0" <= text[0] <= "9" or text[0] in "$%":
+
+    for token_type, token_loc in MnemonicTokenizer.scan(mnem_loc):
+        text = token_loc.text
+        match token_type:
+            case MnemonicToken.identifier:
+                placeholder = placeholder_map.get(text)
+                if placeholder is None:
+                    yield text
+                elif text in seen_placeholders:
+                    # In theory we could support repeated placeholders, but the only
+                    # meaning that would make sense is that they would all match the
+                    # same mode entry or expression and I don't know of any situation
+                    # in which that would be a useful feature.
+                    collector.error(
+                        f'placeholder "{text}" occurs multiple times in mnemonic',
+                        location=(token_loc, seen_placeholders[text]),
+                    )
+                else:
+                    yield placeholder
+                    seen_placeholders[text] = token_loc
+            case MnemonicToken.number:
                 try:
                     value, width = parse_int(text)
                 except ValueError as ex:
-                    collector.error(f"{ex}", location=mnem_elem)
+                    collector.error(f"{ex}", location=token_loc)
                 else:
                     yield int_reference(value, IntType.u(width))
-            else:
+            case MnemonicToken.operator:
                 yield text
-        elif text in seen_placeholders:
-            # In theory we could support repeated placeholders, but the only
-            # meaning that would make sense is that they would all match the
-            # same mode entry or expression and I don't know of any situation
-            # in which that would be a useful feature.
-            collector.error(
-                f'placeholder "{text}" occurs multiple times in mnemonic',
-                location=(mnem_elem, seen_placeholders[text]),
-            )
-        else:
-            yield placeholder
-            seen_placeholders[text] = mnem_elem
+            case MnemonicToken.bracket:
+                yield text
+            case MnemonicToken.separator:
+                yield text
+            case MnemonicToken.other:
+                collector.error(f"invalid token: {text}", location=token_loc)
+            case _:
+                bad_type(token_type)
